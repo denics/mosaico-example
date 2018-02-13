a28963fd2eb8f4d00ab982a4a8e6bb9ab02c6fc6
Denis Pitzalis <d.pitzalis@unesco.org>
2018-02-13 17:55:16 +0100

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount Mosaico::Engine => '/mosaico'
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    mount Mosaico::Engine => '/mosaico'
+    
+    get 'mosaico/index'
+ 
+  root 'mosaico#index'
+    
 end
 
 Mosaico::Engine.routes.draw do
-  resources :projects
-  get '/projects/:template_name/new', to: 'projects#new'
+  get 'mosaico/index'
 
-  resources :templates, param: :template_name, only: [:show]
-
-  scope :images, as: :mosaico_image do
-    get '/(:id)', to: 'images#show'
-    post '/', to: 'images#create'
-    delete '/:id', to: 'images#destroy'
-  end
+    resources :projects
+    
+    get '/projects/:template_name/new', to: 'projects#new'
+    
+    resources :templates, param: :template_name, only: [:show]
+    
+    scope :images, as: :mosaico_image do
+        get '/(:id)', to: 'images#show'
+        post '/', to: 'images#create'
+        delete '/:id', to: 'images#destroy'
+    end
 end
